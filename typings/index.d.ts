3b5bbd9c1d14dc2d307328d4ffd24e32a7b8a652
Wallace Zeng <rongyao.2006@sis.smu.edu.sg>
2016-11-04 16:45:20 +0800

    Update Typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,5 @@
+/// <reference path="globals/bluebird/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/request/index.d.ts" />
+/// <reference path="globals/restify/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
