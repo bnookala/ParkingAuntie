87177321cc3b6b347513b078d77cba2eb5ac7028
Wallace Zeng <rongyao.2006@sis.smu.edu.sg>
2016-11-04 16:59:44 +0800

    Enable Q&A Bot

---
+++ config.js
@@ -0,0 +1,8 @@
+﻿var config = {}
+
+config.host = process.env.HOST || "https://parkingauntie.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "H2AD40qUut6PQv9Bf50GCs5m3qEYJ9ui3Cp9X86WjH2D5z7TxZ0ErZK6nc53CYbsa4AnM5zzHt1HPijTIVzeow==";
+config.databaseId = "ParkingAuntieDB";
+config.collectionId = "ParkingAuntieCollection";
+
+module.exports = config;
