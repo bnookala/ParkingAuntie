884a0315c0f153ee1ac4b0ca780687e300eb94de
Wallace Zeng <rongyao.2006@sis.smu.edu.sg>
2016-11-04 17:05:08 +0800

    Added DocumentDB

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/bluebird/index.d.ts" />
+/// <reference path="globals/documentdb/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/request/index.d.ts" />
 /// <reference path="globals/restify/index.d.ts" />
